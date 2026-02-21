64782f9aa937f06a036d6573a354111f41b50259
Scott Hodson <scott@keepitswift.com>
2026-02-21 01:27:16 +0100

    Create ABN-Places project

---
+++ ABN-Places/ABN_PlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABN_PlacesApp.swift
+//  ABN-Places
+//
+//  Created by Scott Hodson on 21/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct ABN_PlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
