f5298d44324ea961e472fd2cb23900981b36ce57
Scott Hodson <scott@keepitswift.com>
2026-02-21 01:31:15 +0100

    Add Swift Testing target

---
+++ ABN-PlacesTests/ABN_PlacesTests.swift
@@ -0,0 +1,16 @@
+//
+//  ABN_PlacesTests.swift
+//  ABN-PlacesTests
+//
+//  Created by Scott Hodson on 21/02/2026.
+//
+
+import Testing
+
+struct ABN_PlacesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
